42d241f5f9cc5683415e9768307ec6c9eebe1bde
Rakesh Halder <135441929+rocr3@users.noreply.github.com>
2023-09-06 07:10:38 +0530

    Update info.py

--- info.py
+++ info.py
@@ -12,8 +12,8 @@ def is_enabled(value, default):
 
 # Bot information
 SESSION = environ.get('SESSION', 'Media_search')
-API_ID = int(environ.get('API_ID', '11697473'))
-API_HASH = environ.get('API_HASH', '2b15f70f6cfd6064ab3aa681fd98914c')
+API_ID = int(environ.get('API_ID', ''))
+API_HASH = environ.get('API_HASH', '')
 BOT_TOKEN = environ.get('BOT_TOKEN', '')
 
 #Port
