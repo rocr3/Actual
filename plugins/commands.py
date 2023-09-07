ed6beb5feb3b3cde0601df8ebc0fa51c2f68e745
Rakesh Halder <135441929+rocr3@users.noreply.github.com>
2023-09-07 06:25:40 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -246,7 +246,7 @@ async def start(client, message):
         chat_id=message.from_user.id,
         file_id=file_id,
         caption=f_caption,
-        reply_markup=InlineKeyboardMarkup( [ [ InlineKeyboardButton('🚸 ᴅᴇʟᴇᴛᴇ', callback_data='close_data') ] ] ),
+        reply_markup=InlineKeyboardMarkup( [ [ InlineKeyboardButton('Request', url='https://t.me/+LL0gCYlzDnRjYmY1') ] ] ),
         protect_content=True if pre == 'filep' else False,
         )
                     
