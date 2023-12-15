70af4c6ec28d0776c6ef47107d35f1af474cd23c
Rakesh Halder <135441929+rocr3@users.noreply.github.com>
2023-12-15 19:40:33 +0530

    Update info.py

--- info.py
+++ info.py
@@ -75,5 +75,5 @@ if AUTO_DELETE == "True":
     AUTO_DELETE = True
 
 #Sample
-SHORTNER_SITE = "krishnalink.com"
-SHORTNER_API = "7c1ee87027dae0da9eb6fbe0707240a8ddc54141"
+SHORTNER_SITE = "ziplinker.net"
+SHORTNER_API = "55a9b63bafac2a44eb704f938b7ad572057b6fa6"
